bfcfab95242e9f7d5277158d4417fb3d6d69b571
dawidj126 <dawidj126@gmail.com>
2024-06-25 21:56:38 +0200

    20240625

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,24 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose, { MongooseError } from "mongoose";
+import e from "express";
 
 const app = express();
 
 app.get("/", (request, response) => {
 	console.log(request);
-    return response.status(234).send('Test')
+	return response.status(234).send("Test");
 });
 
 app.listen(PORT, () => {
 	console.log(`App is listening to port: ${PORT}`);
 });
+
+mongoose
+	.connect(mongoDBURL)
+	.then(() => {
+		console.log('App connected to database');
+	})
+	.catch((error) => {
+		console.log(error);
+	});
