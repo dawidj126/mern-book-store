9d2ff31708d1e843948e52065cdea1f20b5fe616
dawidj126 <dawidj126@gmail.com>
2024-07-23 23:02:36 +0200

    configure frontend

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,25 @@
-import express, { request } from "express";
+import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
-import mongoose, { MongooseError } from "mongoose";
-import e from "express";
-import { Book } from "./models/bookModel.js";
-import booksRoute from "./routes/booksRoute.js"
+import mongoose from "mongoose";
+import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
 
 // Middleware for parsing request body
 app.use(express.json());
+
+// Middleware for handling CORS policy
+// Option 1: Allow All origins with Default of cors(*)
+app.use(cors());
+// Option 2: Allow Custom Origins
+// app.use(
+// 	cors({
+// 		origin: "http://localhost:3000",
+// 		methods: ["GET", "POST", "PUT", "DELETE"],
+// 		allowedHeaders: ["Content-Type"],
+// 	})
+// );
 
 app.get("/", (request, response) => {
 	console.log(request);
